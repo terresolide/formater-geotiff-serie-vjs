18a52ce5349254398aaf8e01711afaf1640dd347
epointal <elisabeth.pointal@free.fr>
2018-07-17 12:21:54 +0200

    #6 ajout interaction

--- src/leaflet.colorscale-control.js
+++ src/leaflet.colorscale-control.js
@@ -1,12 +1,15 @@
 /**
- * 
+ * A control layer to display interactive colorscale caption
+ * @author epointal
+ * @module L.Control.ColorscaleControl
+ * @requires plotty
+ * @requires leaflet
  */
-/* global L */
-
-// A layer control which provides for layer groupings.
-// Author: Ishmael Smyrnow
 var plotty = require('plotty')
 
+if (typeof L === 'undefined'){
+  var L = require('leaflet')
+}
 L.Control.ColorscaleControl = L.Control.extend({
     options: {
         position: 'bottomright',
@@ -15,6 +18,7 @@ L.Control.ColorscaleControl = L.Control.extend({
     _displayMin: 0,
     _displayMax: 1,
     _canvas: null,
+    _tooltip: null,
     initialize: function (portrayal) {
       this.setPortrayal(portrayal)
     },
@@ -34,17 +38,7 @@ L.Control.ColorscaleControl = L.Control.extend({
       this._displayMax = max
     },
     onAdd: function(map){
-      var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-colorscale');
-      this._tooltip = L.DomUtil.create('div', 'leaflet-colorscale-tooltip hidden')
-      container.append(this._tooltip)
-      this._canvas = L.DomUtil.create('canvas')
-      L.DomEvent.on(this._canvas, 'mouseover', this._displayTooltip, this)
-      L.DomEvent.on(this._canvas, 'mousemove', this._displayTooltip, this)
-      container.append(this._canvas)
-      this._canvas.width = 256
-      this._canvas.height = 1
-      plotty.renderColorScaleToCanvas(this._colorscale, this._canvas)
-      return container
+      return this._initLayout()
     },
     onRemove: function () {
       L.DomEvent.off(this._canvas, 'mouseover', this._displayTooltip, this)
@@ -52,12 +46,44 @@ L.Control.ColorscaleControl = L.Control.extend({
       this._canvas.remove()
       this._canvas = null
     },
+    _initLayout: function () {
+      var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-colorscale');
+      
+      //add tooltip
+      this._tooltip = L.DomUtil.create('div', 'leaflet-colorscale-tooltip hidden')
+      container.append(this._tooltip)
+      
+      //add canvas
+      this._canvas = L.DomUtil.create('canvas')
+      L.DomEvent.on(this._canvas, 'mouseover', this._displayTooltip, this)
+      L.DomEvent.on(this._canvas, 'mousemove', this._displayTooltip, this)
+      L.DomEvent.on(this._canvas, 'mouseout', this._hideTooltip, this)
+      this._canvas.width = 256
+      this._canvas.height = 1
+      plotty.renderColorScaleToCanvas(this._colorscale, this._canvas)
+      container.append(this._canvas)
+ 
+      //stop event propagation on container
+      L.DomEvent.disableClickPropagation(container);
+      L.DomEvent.on(container, 'wheel', L.DomEvent.stopPropagation);
+      return container
+    },
     _displayTooltip: function (evt) {
       console.log(evt)
-      this._tooltip.innerHtml = 'machin'
+      this._tooltip.innerHTML = 'machin'
+      this._tooltip.style.opacity = 0.9
       var pos = this._canvas.getBoundingClientRect()
       console.log(pos)
       console.log(evt.clientX - pos.left)
+      var posX = evt.clientX - pos.left
+      this._tooltip.style.left = (posX - 20) + "px"
+      var value = posX * (this._displayMax - this._displayMin) / pos.width + this._displayMin
+      
+      //@todo affichage petit et grand nombre!! avec 3 chiffres de précision max
+      this._tooltip.innerHTML = (Math.round(value * 1000) / 1000)
+    },
+    _hideTooltip: function (evt) {
+      this._tooltip.style.opacity = 0
     }
 })
 
