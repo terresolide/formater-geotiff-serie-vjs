43246ca4c374ec5b4c0b0602d67341fe4db2d818
elisabethP <elisabeth.pointal@free.fr>
2018-07-29 19:44:50 +0200

    add screenshot

--- src/main.js
+++ src/main.js
@@ -26,31 +26,30 @@ import FormaterGraph from './formater-graph.vue';
 import AerisTheme from '../../aeris-commons-components-vjs/src/aeris-theme/aeris-theme.vue';
 
 ljs.addAliases({
-	dep: [
-	   'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', 
-	  //'https://use.fontawesome.com/releases/v5.1.0/css/all.css',
-	   // leaflet fontawesome marker
-	   //--------------------------
-	 //icon marker with font awesome CSS
-	    //-----------------------------
-	    'https://api.poleterresolide.fr/lib/awesome-marker/leaflet.awesome-markers.css',
-		 //leaflet
-		//------------
-		'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.css',
-		//regiter element
-		//-------------
-	  'https://cdnjs.cloudflare.com/ajax/libs/document-register-element/1.4.1/document-register-element.js',
-	  'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment-with-locales.min.js',
-	  'https://cdnjs.cloudflare.com/ajax/libs/moment-range/3.0.3/moment-range.min.js'
-	  ]
+    dep: [
+       'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', 
+       //'https://use.fontawesome.com/releases/v5.1.0/css/all.css',
+       // leaflet fontawesome marker
+       //--------------------------
+       //icon marker with font awesome CSS
+       //-----------------------------
+       'https://api.poleterresolide.fr/lib/awesome-marker/leaflet.awesome-markers.css',
+       //leaflet css
+       //------------
+       'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.css',
+       //regiter element
+       //-------------
+       'https://cdnjs.cloudflare.com/ajax/libs/document-register-element/1.4.1/document-register-element.js',
+       'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment-with-locales.min.js',
+       'https://cdnjs.cloudflare.com/ajax/libs/moment-range/3.0.3/moment-range.min.js'
+      ]
 })
 ljs.load('dep', function() {
   Vue.customElement('formater-graph', FormaterGraph);
   Vue.customElement('aeris-theme', AerisTheme);
   Vue.customElement('geotiff-serie-control', GeotiffSerieControl);
-
- // Vue.customElement('formater-draggable-block', FormaterDraggableBlock)
+  // Vue.customElement('formater-draggable-block', FormaterDraggableBlock)
   Vue.customElement('formater-map', Map);
-	// Vue.customElement('formater-test', Test);
+  // Vue.customElement('formater-test', Test);
 })
 
